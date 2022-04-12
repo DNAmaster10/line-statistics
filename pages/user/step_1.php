5e99131121fb16cf71dc21fb4431db1e5af382b4
DNAmaster10 <louishollobon@outlook.com>
2022-04-12 23:46:45 +0100

    Update step_1.php

--- pages/user/step_1.php
+++ pages/user/step_1.php
@@ -16,9 +16,11 @@
     <form action="./step_2.php" method="POST">
         <p>Line: </p><input id="line_name_entry" type="text" name="line_name" placeholder="e.g: Zephyr">
         <p>Enter either the counter ID (found on a sign within the counter) OR enter names of the two stations the counter sits between below</p><br>
-        <p>Counter ID: <input type="text" id="counter_id_entry_box"
-        <input type="text" id="station1_entry" name="station1" placeholder="e.g: ZN1">
-        <input type="text" id="station2_entry" name="station2" placeholder="e.g: ZN2">
+        <p>Counter ID: <input type="text" id="counter_id_entry_box" name="counter_id" placeholder="e.g: 63">
+        <p>OR:</p><br>
+        <p>Station 1: </p><input type="text" id="station1_entry" name="station1" placeholder="e.g: ZN1">
+        <p>Station 2: </p><input type="text" id="station2_entry" name="station2" placeholder="e.g: ZN2"><br>
+        <p>Riders since last check</p><input type="text" name="rider_ammount" placeholder="e.g: 12">
         <input type="submit" value="Next">
     </form>
     <p><?php if (isset($error_message)) {echo $error_message;} ?></p>
